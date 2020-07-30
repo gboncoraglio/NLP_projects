4fcab7d2d5cf5e5811f93ce5ec8fdf3613b9167f
Gabriele Boncoraglio <65842059+gboncoraglio@users.noreply.github.com>
2020-07-29 23:10:35 -0700

    Update vocab.py

--- machine_translation/neural_machine-translation/vocab.py
+++ machine_translation/neural_machine-translation/vocab.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
-CS224N 2018-19: Homework 4
-vocab.py: Vocabulary Generation
-Pencheng Yin <pcyin@cs.cmu.edu>
-Sahil Chopra <schopra8@stanford.edu>
 
 Usage:
     vocab.py --train-src=<file> --train-tgt=<file> [options] VOCAB_FILE
